826919074067bfe99156e20adf22b5d0ba588ea0
Brian lorx Robertson <raposa.msscripting@gmail.com>
2011-02-26 21:55:16 -0700

    Rearrange

--- mssbot.hs
+++ mssbot.hs
@@ -20,140 +20,16 @@ import qualified System.IO.UTF8 as I
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.UTF8 as U
 
+freenode = defaultConfig
+  { cAddr = "irc.freenode.net"
+  , cNick = "mssbotdev"
+  , cChannels = ["##mssdev", "#maelstrom"]
+  , cEvents = events
+  }
+
 tellFile = "/var/tmp/mssbottelllist"
 googleFile = "/var/tmp/mssbotgoogle"
 urlFile = "/var/tmp/mssboturl"
-
-killSpaces :: String -> String
-killSpaces [] = ""
-killSpaces (a:[]) = if a=='\t' || a=='\n' then [] else [a]
-killSpaces (a:b:ss) = if (a == ' ' && b == ' ') || a=='\t' || a=='\n' then killSpaces $ b:ss else a: (killSpaces $ b:ss)
-
-runCmd :: String -> [String] -> IO ()
-runCmd cmd options = do
-	(_, Just force, _, _) <- createProcess (proc cmd options){ std_out = CreatePipe }
-	runForce <- hGetContents force
-	putStrLn runForce
-
-download :: String -> String -> IO()
-download url file = runCmd "curl" ["-sSL", "--user-agent","Mozilla/4.0", "-o",file, url]
-
-getTitle :: String -> IO String
-getTitle url = do
-	putStrLn $ concat ["Getting ", url] 
-	download url urlFile
-	(_, Just kind, _, _) <- createProcess (proc "file" ["-b", urlFile]){ std_out = CreatePipe }
-	kindoffile <- hGetContents kind
-	let ftype = takeWhile (/=' ') kindoffile
-	putStrLn $ concat ["It's a ", ftype, " document"]
-	if ftype == "HTML" || ftype == "xHTML" then do
-	html <- I.readFile urlFile
---	tloop hin
-	let title = killSpaces $ flip stringRegex "(?<=>)[^<]*" $ stringRegex html "<[^>]*[tT][iI][tT][lL][eE][^>]*>[^<]*<[^>]*/[^>]*[tT][iI][tT][lL][eE][^>]*>"
-	if length title > 0 then return title else return ""
-	else return ""
---	where
---		tloop html = do
---			let title = killSpaces $ flip stringRegex "(?<=>)[^<]*" $ stringRegex html "<[^>]*[tT][iI][tT][lL][eE][^>]*>[^<]*<[^>]*/[^>]*[tT][iI][tT][lL][eE][^>]*>"
---			let title0 = stringRegex html "<[^>]*[tT][iI][tT][lL][eE][^>]*>[^<]*<[^>]*/[^>]*[tT][iI][tT][lL][eE][^>]*>"
---			putStrLn title0
---			let title1 = stringRegex title0 "(?<=>)[^<]*"
---			putStrLn title1
---			let title = killSpaces title1 
---			if length title > 0 then return title else return ""
-
-tell :: MIrc -> B.ByteString -> String -> IO()
-tell s chan nik = do
-	isTellFile <- doesFileExist tellFile
-	if isTellFile then do
-	all <- I.readFile tellFile
-	let messages = map (\a -> read a :: (String, String, String, String)) $ lines all
-	let tells = map (\(a,b,c,t) -> concat $ [t, " UTC <",b,"> tell ",nik," ",c]) $ filter (\(a,b,c,t) -> isPrefixOf (lower a) (lower nik)) messages
-	goTell s chan tells
-	I.writeFile tellFile $ unlines $ map (show) $ filter (\(a,b,c,t) -> not (isPrefixOf (lower a) (lower nik))) messages
-	else I.writeFile tellFile ""
-	where
-		goTell _ _ [] = return ()
-		goTell s chan (t:ts) = do 
-			sendMsg s chan $ U.fromString t
-			goTell s chan ts
-
-dieMulti :: String -> Int
-dieMulti a = if length b == 0 then 1 else read b ::Int
-	where b = stringRegex a "([0-9]+)?(?=d)"
-
-dieOffset :: String -> Int
-dieOffset a = if length b == 0 then 0 else if head b == '+' then read $ drop 1 b ::Int else read b ::Int
-	where b = stringRegex a "(\\+|-)[0-9]+"
-
-dieLoop :: String -> Int
-dieLoop a = if length b == 0 then 1 else read b ::Int
-	where b = stringRegex a "[0-9]+(?=\\|)"
-
-dieD :: String -> Int
-dieD a = if b == "%" then 100 else read b ::Int
-	where b = stringRegex a "(?<=d)([0-9]+|%)"
-
-roll :: Int -> IO Int
-roll d = getStdRandom (randomR (1,d))
-
-droll :: (Int, Int, Int, Int) -> IO [Int]
-droll (_, _, _, 0) = return []
-droll (d, multi, offset, num) = do
-	m <- (dmulti d multi)
-	m2 <- droll (d, multi, offset, num-1)
-	return $ (m+offset):m2
-	where
-		dmulti _ 0 = return 0
-		dmulti dm mul = do
-			r <- roll d
-			r2 <- dmulti dm (mul-1)
-			return $ r+r2
-
-collapseroll :: [IO [Int]] -> IO String
-collapseroll [] = return ""
-collapseroll (i:is) = do
-	a <- i
-	b <- collapseroll is
-	return $ unwords [(unwords $ map (show) a),b]
-
-fToC :: Int -> Int
-fToC f = round $ (*) (5 / 9) $ flip (-) 32 $ fromIntegral f
-
-dropCommand :: B.ByteString -> B.ByteString
-dropCommand b = B.drop 1 $ B.dropWhile (/= ' ') b
-
-stringDropCmd :: B.ByteString -> String
-stringDropCmd = U.toString . dropCommand
-
-stringRegex :: String -> String -> String
-stringRegex orig regex = orig =~ regex
-
-listRegex :: String -> String -> [[String]]
-listRegex orig regex = orig =~ regex :: [[String]]
-
-boolRegex :: String -> String -> Bool
-boolRegex orig regex = orig =~ regex :: Bool
-
-spaceToPlus :: String -> String
-spaceToPlus = map (\a -> if a==' ' then '+' else a)
-
-address :: String -> String -> B.ByteString
-address nik s = U.fromString $ concat [nik, ": ", s]
-
-secondBuffer :: String -> String
-secondBuffer [] = "00"
-secondBuffer (s:[]) = '0':s:""
-secondBuffer s = s
-
-time :: Int -> String
-time i = concat [show minutes, ":", secondBuffer $ show seconds]
-	where
-		minutes = floor $ flip (/) 60 $ fromIntegral i
-		seconds = i - (minutes*60)
-
-lower :: String -> String
-lower = map (toLower)
 
 onMessage :: EventFunc
 onMessage s m
@@ -217,15 +93,130 @@ onMessage s m
         msg = mMsg m
         nik = fromJust $ mNick m
         nick = U.toString nik
-        
-events = [(Privmsg onMessage)]
 
-freenode = defaultConfig
-  { cAddr = "irc.freenode.net"
-  , cNick = "mssbotdev"
-  , cChannels = ["##mssdev", "#maelstrom"]
-  , cEvents = events
-  }
+getTitle :: String -> IO String
+getTitle url = do
+	putStrLn $ concat ["Getting ", url] 
+	download url urlFile
+	(_, Just kind, _, _) <- createProcess (proc "file" ["-b", urlFile]){ std_out = CreatePipe }
+	kindoffile <- hGetContents kind
+	let ftype = takeWhile (/=' ') kindoffile
+	putStrLn $ concat ["It's a ", ftype, " document"]
+	if ftype == "HTML" || ftype == "xHTML" then do
+	html <- I.readFile urlFile
+	let title = killSpaces $ flip stringRegex "(?<=>)[^<]*" $ stringRegex html "<[^>]*[tT][iI][tT][lL][eE][^>]*>[^<]*<[^>]*/[^>]*[tT][iI][tT][lL][eE][^>]*>"
+	if length title > 0 then return title else return ""
+	else return ""
+
+tell :: MIrc -> B.ByteString -> String -> IO()
+tell s chan nik = do
+	isTellFile <- doesFileExist tellFile
+	if isTellFile then do
+	all <- I.readFile tellFile
+	let messages = map (\a -> read a :: (String, String, String, String)) $ lines all
+	let tells = map (\(a,b,c,t) -> concat $ [t, " UTC <",b,"> tell ",nik," ",c]) $ filter (\(a,b,c,t) -> isPrefixOf (lower a) (lower nik)) messages
+	goTell s chan tells
+	I.writeFile tellFile $ unlines $ map (show) $ filter (\(a,b,c,t) -> not (isPrefixOf (lower a) (lower nik))) messages
+	else I.writeFile tellFile ""
+	where
+		goTell _ _ [] = return ()
+		goTell s chan (t:ts) = do 
+			sendMsg s chan $ U.fromString t
+			goTell s chan ts
+
+killSpaces :: String -> String
+killSpaces [] = ""
+killSpaces (a:[]) = if a=='\t' || a=='\n' then [] else [a]
+killSpaces (a:b:ss) = if (a == ' ' && b == ' ') || a=='\t' || a=='\n' then killSpaces $ b:ss else a: (killSpaces $ b:ss)
+
+runCmd :: String -> [String] -> IO ()
+runCmd cmd options = do
+	(_, Just force, _, _) <- createProcess (proc cmd options){ std_out = CreatePipe }
+	runForce <- hGetContents force
+	putStrLn runForce
+
+download :: String -> String -> IO()
+download url file = runCmd "curl" ["-sSL", "--user-agent","Mozilla/4.0", "-o",file, url]
+
+droll :: (Int, Int, Int, Int) -> IO [Int]
+droll (_, _, _, 0) = return []
+droll (d, multi, offset, num) = do
+	m <- (dmulti d multi)
+	m2 <- droll (d, multi, offset, num-1)
+	return $ (m+offset):m2
+	where
+		dmulti _ 0 = return 0
+		dmulti dm mul = do
+			r <- roll d
+			r2 <- dmulti dm (mul-1)
+			return $ r+r2
+
+dieMulti :: String -> Int
+dieMulti a = if length b == 0 then 1 else read b ::Int
+	where b = stringRegex a "([0-9]+)?(?=d)"
+
+dieOffset :: String -> Int
+dieOffset a = if length b == 0 then 0 else if head b == '+' then read $ drop 1 b ::Int else read b ::Int
+	where b = stringRegex a "(\\+|-)[0-9]+"
+
+dieLoop :: String -> Int
+dieLoop a = if length b == 0 then 1 else read b ::Int
+	where b = stringRegex a "[0-9]+(?=\\|)"
+
+dieD :: String -> Int
+dieD a = if b == "%" then 100 else read b ::Int
+	where b = stringRegex a "(?<=d)([0-9]+|%)"
+
+roll :: Int -> IO Int
+roll d = getStdRandom (randomR (1,d))
+
+collapseroll :: [IO [Int]] -> IO String
+collapseroll [] = return ""
+collapseroll (i:is) = do
+	a <- i
+	b <- collapseroll is
+	return $ unwords [(unwords $ map (show) a),b]
+
+fToC :: Int -> Int
+fToC f = round $ (*) (5 / 9) $ flip (-) 32 $ fromIntegral f
+
+dropCommand :: B.ByteString -> B.ByteString
+dropCommand b = B.drop 1 $ B.dropWhile (/= ' ') b
+
+stringDropCmd :: B.ByteString -> String
+stringDropCmd = U.toString . dropCommand
+
+stringRegex :: String -> String -> String
+stringRegex orig regex = orig =~ regex
+
+listRegex :: String -> String -> [[String]]
+listRegex orig regex = orig =~ regex :: [[String]]
+
+boolRegex :: String -> String -> Bool
+boolRegex orig regex = orig =~ regex :: Bool
+
+spaceToPlus :: String -> String
+spaceToPlus = map (\a -> if a==' ' then '+' else a)
+
+address :: String -> String -> B.ByteString
+address nik s = U.fromString $ concat [nik, ": ", s]
+
+secondBuffer :: String -> String
+secondBuffer [] = "00"
+secondBuffer (s:[]) = '0':s:""
+secondBuffer s = s
+
+time :: Int -> String
+time i = concat [show minutes, ":", secondBuffer $ show seconds]
+	where
+		minutes = floor $ flip (/) 60 $ fromIntegral i
+		seconds = i - (minutes*60)
+
+lower :: String -> String
+lower = map (toLower)
+
+events = [(Privmsg onMessage)]
 
 main = do
   connect freenode False True
+
